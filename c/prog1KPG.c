52373f3861f60f287e6bfe5b37d2fa6b7959a716
Kaleb G <kaleb.gilson@und.edu>
2023-02-03 09:04:30 -0600

    C project part 2

--- c/prog1KPG.c
+++ c/prog1KPG.c
@@ -25,11 +25,32 @@ void partOne() {
     int seconds = totalSeconds-(minutes*60)-(hours*3600); // subtract minutes and hours (in seconds) from total seconds
 
     printf("\n" "This is %d hours %d minutes and %d seconds.", hours, minutes, seconds);
-    printf("\n");
 }
 
 void partTwo() {
+    int quarters, dimes, nickles, pennies;
 
+    printf("\n\nQuarters:");
+    scanf("%d", &quarters);
+
+    printf("Dimes:");
+    scanf("%d", &dimes);
+
+    printf("Nickles:");
+    scanf("%d", &nickles);
+
+    printf("Pennies:");
+    scanf("%d", &pennies);
+
+    double total = 0; // Define the "total" variable
+
+    // Multiply the amount of coins by the amount the coins are worth, add it to the total
+    total+=(quarters*0.25);
+    total+=(dimes*0.1);
+    total+=(nickles*0.05);
+    total+=(pennies*0.01);
+
+    printf("This is equal to $%.2f", total); // Print the output
 }
 
 void partThree() {
